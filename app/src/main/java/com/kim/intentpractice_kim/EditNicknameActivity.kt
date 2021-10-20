9b5302fed20f0041d688aed897fc95d9b910f192
KJH <daeding7@naver.com>
2021-10-20 19:57:20 +0900

    닉네임 입력용 별개 화면 추가

---
+++ app/src/main/java/com/kim/intentpractice_kim/EditNicknameActivity.kt
@@ -0,0 +1,11 @@
+package com.kim.intentpractice_kim
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNicknameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_nickname)
+    }
+}
