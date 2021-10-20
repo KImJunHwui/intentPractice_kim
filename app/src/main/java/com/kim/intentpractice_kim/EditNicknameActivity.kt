e95195445c7011daa021548017d3bbc3d5858485
KJH <daeding7@naver.com>
2021-10-20 20:40:32 +0900

    닉네임 변경 화면 -> 입력 후 완료 클릭시 데이터 들고 돌아가기 -> 돌아간 메인화면에서 데이터 반영 별도 작업 필요

--- app/src/main/java/com/kim/intentpractice_kim/EditNicknameActivity.kt
+++ app/src/main/java/com/kim/intentpractice_kim/EditNicknameActivity.kt
@@ -1,5 +1,6 @@
 package com.kim.intentpractice_kim
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_edit_nickname.*
@@ -10,6 +11,20 @@ class EditNicknameActivity : AppCompatActivity() {
         setContentView(R.layout.activity_edit_nickname)
 
         okBtn.setOnClickListener {
+
+
+//  입력한 닉네임 -> 변수로 지정
+            val inputNewNickname = newNicknameEdt.text.toString()
+
+//            입력한 닉네임을 가지고 복귀해야 한다
+
+//            입력 닉네임을 담아주기 위한 용도로만 사용하는 Intent
+
+            val resultIntent = Intent()
+        resultIntent.putExtra("nickname", inputNewNickname)
+            setResult(RESULT_OK,resultIntent)
+            finish()
+
 
 
 
